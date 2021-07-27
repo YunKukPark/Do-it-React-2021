f67cfd31ac31f8e5c46205860cf7545964758123
YunKukPark <a01043340999@gmail.com>
2021-07-27 16:17:59 +0900

    3-1 예제 :: 컴포넌트 제작해보기

---
+++ do-it-example/src/03/TodaysPlan.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class TodaysPlan extends Component {
+  render() {
+    return <div className="message-container">ㅎㅇㅎㅇ</div>;
+  }
+}
+
+export default TodaysPlan;
