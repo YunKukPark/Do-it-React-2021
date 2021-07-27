f8f54ba3e3f5e1aca2810bcfb190e3e26e9cb5d6
YunKukPark <a01043340999@gmail.com>
2021-07-27 16:25:13 +0900

    Rename App.js to App.jsx

---
+++ do-it-example/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PropsComponent from './03/PropsComponent';
+
+class App extends Component {
+  render() {
+    return <PropsComponent name="안녕하세요" />;
+  }
+}
+
+export default App;
