dc134fbbc1f2c8142683fe54205bd961604118b2
YunKukPark <a01043340999@gmail.com>
2021-07-28 20:07:39 +0900

    Study about Component(props)

--- do-it-example/src/App.jsx
+++ do-it-example/src/App.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
-import PropsComponent from './03/PropsComponent';
+import ChildComponent from './03/ChildComponent';
 
 class App extends Component {
   render() {
-    return <PropsComponent name="안녕하세요" />;
+    const arr = [1, 2, 3];
+    const obj = { name: '제목', age: 30 };
+    const node = <h1>NODE</h1>;
+    const myFunc = () => {
+      console.log('메시지');
+    };
+
+    return (
+      <div id="App">
+        <ChildComponent
+          boolValue={true}
+          numValue={1}
+          arrayValue={arr}
+          objValue={obj}
+          nodeValue={node}
+          funcValue={myFunc}
+        />
+      </div>
+    );
   }
 }
 
