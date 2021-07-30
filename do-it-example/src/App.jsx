0e90cd71be7b3fb484c2cc4e33fe999e65ca083c
YunKukPark <a01043340999@gmail.com>
2021-07-30 14:40:10 +0900

    📝 3-3 Study about Component Props

--- do-it-example/src/App.jsx
+++ do-it-example/src/App.jsx
@@ -1,32 +1,15 @@
 import React, { Component } from 'react';
-import BooleanComponent from './03/BooleanComponent';
-import ChildComponent from './03/ChildComponent';
-import ChildComponent2 from './03/ChildComponent2';
+import ChildProperty from './03/ChildProperty';
 
 class App extends Component {
   render() {
-    const arr = [1, 2, 3];
-    const node = <h1>NODE</h1>;
-    const obj = {
-      name: 'yunkuk',
-      age: 20,
-    };
-    const myFunc = () => {
-      console.log('메시지');
-    };
-
     return (
-      <div id="App">
-        <ChildComponent
-          boolValue={true}
-          numValue={1}
-          arrayValue={arr}
-          nodeValue={node}
-          funcValue={myFunc}
-        />
-        <ChildComponent2 objValue={obj} />
-        <BooleanComponent bored />
-        <BooleanComponent />
+      <div className="App">
+        <ChildProperty>
+          <div>
+            <span>자식노드</span>
+          </div>
+        </ChildProperty>
       </div>
     );
   }
