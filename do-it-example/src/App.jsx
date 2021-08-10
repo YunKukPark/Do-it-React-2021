c1b24f6ceb586b646ab3aeb1a81dda1c9a0b6690
YunKukPark <a01043340999@gmail.com>
2021-08-10 15:31:32 +0900

    🚧 Add Materialize css/scss Files

--- do-it-example/src/App.jsx
+++ do-it-example/src/App.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import Counter from './03/Counter3';
-import ScrollSpy from './03/ScrollSpy';
+import './sass/materialize.scss';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Counter />
-        <ScrollSpy />
+      <div>
+        <nav>
+          <div className="nav-wraaper">
+            <div className="nav-contents">두잇! 리액트 시작하기</div>
+          </div>
+        </nav>
+        <h1>머티리얼 CSS</h1>
       </div>
     );
   }
